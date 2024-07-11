03ebff80de21059dfd29bb40738306ab046df50b
Theoryia <theokinder@hotmail.co.uk>
2024-07-11 01:09:37 +0100

    back again

--- src/app/myStack.js
+++ src/app/myStack.js
@@ -88,7 +88,7 @@ function createVisualization(data) {
     window.addEventListener('resize', () => {
         clearTimeout(resizeTimeout);
         resizeTimeout = setTimeout(() => {
-            document.location.reload();
+            window.location.reload();
         }, 500);
     });
 }
